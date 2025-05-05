34ff6936cad12bce14efda21976a9b3143e90bec
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-05 12:32:02 +0300

    working with numbers and checking out numbers methods like absolute,round etc and type casting

---
+++ working _with_numbers.py
@@ -0,0 +1,33 @@
+# working with numbers
+# integer is a whole number while float is a decimal
+# order of operations bodmas works
+# comparisons
+# equal 3==2
+# not equal 3!=2
+# num_1 =3
+# num_2 =4
+# print(num_1==num_2)
+# num =3
+
+# check the data type 
+# print(type(num))
+# print(3%2)
+
+# order of operations
+# print(3*(2+1))
+
+# direct multiplying
+numOne=2
+numOne*=10
+print(numOne)
+
+# This round off to i dp
+print(round(3.75,1))
+
+# this are strings
+num_1='100' 
+num_2='200'
+# casting use int() as used in this example
+num_1 =int(num_1)
+num_2=int(num_2)
+print(num_1+num_2)
