af017d873c9cae4ebed7c6652ce3aa16917f8526
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-05 15:11:15 +0300

    working with lists ,sets,tuples and the methods to modify them

---
+++ working with_lists_tuples_sets.py
@@ -0,0 +1,30 @@
+# allows us to work with a list of values
+# slicing [0:2]
+list_1 =['history','maths','english']
+list_2=list_1
+# nums=[3,5,79,9,7]
+# print(courses[0:1])
+
+# list methods
+# adding item
+# courses.insert(0,"art")
+# print(courses)
+# # removing items
+# courses.pop()
+# print(courses)
+# nums.sort()
+# print(nums)
+# sorted_courses=sorted(courses)
+# print(sorted_courses)
+
+# finding the index
+# print(courses.index('math'))
+# print('math'in courses)
+# for index, course in enumerate(courses,starts=1)
+
+
+# tupples
+# we cant modify tupples they are immutable
+tuple_1 =('history', 'maths', 'english')
+tuple_2=tuple_1
+# sets this are values that are not ordered 
