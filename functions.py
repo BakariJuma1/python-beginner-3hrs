499141234cd367bd6f4174f6cdc892d8bcded0e0
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-05 22:33:32 +0300

    more examples in functions

--- functions.py
+++ functions.py
@@ -5,10 +5,27 @@
 
 # print(hello_func('Hi',name='bakari')   ) 
 
-def greeting(name):
-    return(f"hello,{name}")
+# def greeting(name):
+#     return(f"hello,{name}")
     
-print(greeting("bakari"))    
+# print(greeting("bakari"))    
 
 def  greet_programmer():
-    print("Hello, programmer!")
+    return("Hello, programmer!")
+print(greet_programmer())
+
+def greet(name):
+    return(f"Hello ,{name}")
+print(greet('bakari'))
+
+def greet_with_default(name='juma'):
+    return(f"Hello,{name}")
+print(greet_with_default())
+
+def add(a,b):
+    return(a+b)
+print(add(3,2))
+
+def halve(a):
+    return(a/2)
+print(halve(10))
