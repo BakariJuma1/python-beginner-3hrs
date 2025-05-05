e8c4db9466f069089c3f1733dfdef4edf612ba49
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-05 21:20:30 +0300

    what evaluates to false how to run if and else and elif statements

---
+++ working_with_conditional_statements.py
@@ -0,0 +1,29 @@
+# conditionals
+# == equality
+language='java'
+if language=='python':
+      print('language is python')
+elif language=='java':
+      print('language is java')  
+elif language=='js':
+      print('language is js')            
+else:
+      print('no match')      
+
+    
+# and 
+#    or 
+# not  
+user = 'admin'
+logged_in=True
+
+# if user=='admin' and logged_in:
+#       print('admin page')
+# else:
+#       print('bad creds')   
+
+if  not logged_in: 
+   print("please log in")
+else:
+      print("you are logged in")   
+# false values false none zero empty sequence empty mapping 
