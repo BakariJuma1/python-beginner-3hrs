d8d27e1a0be63aa61423b2d6b52a8da330e3e059
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-05 22:24:19 +0300

    understanding functions and their return statements

---
+++ functions.py
@@ -0,0 +1,14 @@
+# we use def keyword
+
+# def hello_func(greeting,name='you'):
+#     return('{} ,{}'.format(greeting,name))
+
+# print(hello_func('Hi',name='bakari')   ) 
+
+def greeting(name):
+    return(f"hello,{name}")
+    
+print(greeting("bakari"))    
+
+def  greet_programmer():
+    print("Hello, programmer!")
--- working_ with _loops.py
+++ working_ with _loops.py
@@ -1,24 +1,24 @@
 # break break from loop
 # continoues the loop
-nums =[1,2,3,4,5,6]
-for num in nums:
-    if num==3:
-        print('found it')
-        # break
-        continue
-    print(num)
+# nums =[1,2,3,4,5,6]
+# for num in nums:
+#     if num==3:
+#         print('found it')
+#         # break
+#         continue
+#     print(num)
     
 # loop eithin loop    # 
 # use range for a certain number of times you want to go through a looop 
-for i in range(1,10):
-    print(i)
+# for i in range(1,10):
+#     print(i)
 
 
 
     # while loops 
-x = 0
-while True:
-    if x==5:
-        break
-    print(x)   
-    x+=1 
+# x = 0
+# while True:
+#     if x==5:
+#         break
+#     print(x)   
+#     x+=1 
