4036a1172e29edc7331c3438fe0009d6e3c77d85
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-05 15:22:58 +0300

    working with dictionaries

---
+++ working_with_dictionaries.py
@@ -0,0 +1,10 @@
+# dictionaries allows use to work with key value pairs where the key is a unique identifier and the value is the data 
+
+student={'name':'bakari','age':25,'courses':'it'}
+# print(student['age'])
+# adding 
+# student.update({'name':'age':26,'phone':839930})
+student['phone']='766883'
+# print(student.get('phone','not found'))
+# print(student)
+print(student.keys)
